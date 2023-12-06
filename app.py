539925183d0e1eaa05d189600410b75d908a3889
songhyeonjun <thdwoqja11@naver.com>
2023-12-06 14:19:36 +0900

    EXE 파일 만들기 주석 추가

--- app.py
+++ app.py
@@ -82,4 +82,28 @@ if __name__ == '__main__':
     app = App(root)
     root.mainloop()
     
+
+
+    # PyInstaller 설치
+        # pip install pyinstaller * 터미널에서 입력
     
+    # EXE 파일 생성
+        # pyinstaller --onefile --windowed 폴더경로/대상파일
+        #     --onefile: 모든 필요한 파일을 하나의 실행 파일로 번들링합니다.
+        #     --windowed: 콘솔 창 없이 GUI 애플리케이션으로 실행되도록 설정합니다.
+    
+    # 생성된 EXE 파일 확인
+        # pyinstaller가 프로세스를 완료하면, dist 폴더 내에 app.exe 파일이 생성됩니다. 이 파일이 최종적으로 생성된 EXE 파일
+        
+    
+    # pyinstaller' 용어가 cmdlet, 함수, 스크립트 파일 또는 실행할 수 있는 프로그램 이름으로 인식되지 않는다' 오류 발생시
+    # 1. pyinstaller 재설치
+    # 2. 환경 변수 확인
+        # pyinstaller가 설치된 경로가 시스템의 환경 변수 PATH에 포함되어 있는지 확인. 
+        # pyinstaller는 일반적으로 Python의 Scripts 폴더 내에 설치됨
+        # ex) C:\Users\p13259\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts
+    
+        # 제어판 → 시스템 및 보안 → 시스템 → 고급 시스템 설정 → 환경 변수.
+        # 시스템 변수 섹션에서 Path 변수를 찾아 편집.
+        # 새로 만들기를 클릭하고 pyinstaller의 경로 (예: C:\Python39\Scripts)를 추가.
+        # 확인을 눌러 변경 사항을 저장.
